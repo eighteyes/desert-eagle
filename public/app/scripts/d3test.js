82396764a115a3ec3fb0a2c7624d29c6fa51f572
Sean Canton <eighteyes@gmail.com>
2013-07-03 15:41:10 -0700

    Fill chrat

--- public/app/scripts/d3test.js
+++ public/app/scripts/d3test.js
@@ -5,7 +5,7 @@ function drawLines(data){
  var lineData = [];
 
   data.wave.forEach( function( t, i ){
-    lineData.push({ y: Math.round((Math.abs(t*100)*100)/100), x: i});
+    lineData.push({ y: Math.round(t*10000)/100, x: i });
   });
 
   var line = d3.svg.line()
@@ -21,14 +21,24 @@ function drawLines(data){
     })
 
   var path = svg
-        .append('path')
-        .attr({
-          'd' : line(lineData),
-          'stroke': 'blue',
-          'stroke-width': 0.25,
-          'fill':'none',
-          });
+    .append('path')
+    .attr({
+      'd' : line(lineData),
+      'stroke': 'blue',
+      'stroke-width': 0.25,
+      'fill':'none',
+    });
 
+  var bds = svg.selectAll('div')
+    .data(lineData)
+      .enter().append('rect')
+      .attr({
+        'x' : function(d, i) { return i; },
+        'y' : 0,
+        'width' : 2,
+        'height' : function(d) { return d.y },
+        'fill' : 'rgba(100,100,200,0.75)'
+      });
 }
 
 d3.json('http://localhost:8000/song', function(data){
